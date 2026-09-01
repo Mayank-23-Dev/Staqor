734b32899ff941bc0ace23bfc896a87b26f45643
Mayank Dev <dmayank2323@gmail.com>
2026-09-01 19:34:39 +0530

    chore: remove obsolete and unused files

--- AB Talk/src/ui/Dashboard_UI/index.ts
+++
@@ -1,5 +0,0 @@
-export { StreakCounter } from './StreakCounter';
-export { TodayTaskCard } from './TodayTaskCard';
-export { ProgressBar } from './ProgressBar';
-export { CompletionStats } from './CompletionStats';
-export { Achievements } from './Achievements';
--- AB Talk/src/ui/Day_UI/index.ts
+++
@@ -1,4 +0,0 @@
-export { DayHeader } from './DayHeader';
-export { TaskDescription } from './TaskDescription';
-export { SubmissionForm } from './SubmissionForm';
-export { SubmittedState } from './SubmittedState';
